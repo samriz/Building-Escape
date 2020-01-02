84371e5aa6166bbfd6544373f9768dacb41e7b82
samriz <samriz@users.noreply.github.com>
2020-01-01 21:11:42 -0600

    Displayed the name of the owner of the component.

--- Source/Building_Escape/WorldPosition.cpp
+++ Source/Building_Escape/WorldPosition.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -18,11 +19,7 @@ UWorldPosition::UWorldPosition()
 void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	UE_LOG(LogTemp, Display, TEXT("This is Display verbosity. It is in grey text."))
-	UE_LOG(LogTemp, Warning, TEXT("This is a Warning."))
-	UE_LOG(LogTemp, Error, TEXT("This is an Error."))
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetName());
 }
 
 
