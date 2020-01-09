001c41d68409d8c6bcefee443881bb66f4fba4b0
samriz <samriz@users.noreply.github.com>
2020-01-08 21:38:54 -0600

    Get door to open slowly upon pressing play without it opening simultaneously like before. User linear interpolation (Lerp) to do this.

--- Source/Building_Escape/OpenDoor.cpp
+++ Source/Building_Escape/OpenDoor.cpp
@@ -9,28 +9,28 @@ UOpenDoor::UOpenDoor()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
 
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	//FRotator CurrentRotation = GetOwner()->GetActorRotation();
-	//CurrentRotation.Yaw = 90.f;
-	FRotator OpenDoor(0.f, 90.f, 0.f);
-	GetOwner()->SetActorRotation(OpenDoor);
 }
-
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString())
+	UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw)
 
-	// ...
+	float CurrentYaw{GetOwner()->GetActorRotation().Yaw};
+	FRotator OpenDoor{0.f, TargetYaw, 0.f};
+	OpenDoor.Yaw = FMath::Lerp(CurrentYaw, TargetYaw, 0.02f);
+	GetOwner()->SetActorRotation(OpenDoor);
+
+	//FRotator CurrentRotation = GetOwner()->GetActorRotation();
+	//CurrentRotation.Yaw = 90.f;
+	//FRotator OpenDoor(0.f, 90.f, 0.f);
+	//GetOwner()->SetActorRotation(OpenDoor);
 }
-
