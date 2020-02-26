eb4f6bdc47cab4187b16cf37d27e1a954d347c2d
samriz <samriz@users.noreply.github.com>
2020-02-26 17:01:05 -0600

    Refactored code by moving pieces of code into its own functions.

--- Source/Building_Escape/Grabber.cpp
+++ Source/Building_Escape/Grabber.cpp
@@ -13,20 +13,26 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
 
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
+	FindPhysicsHandle();
+	SetupInputComponent();
+}
 
+void UGrabber::FindPhysicsHandle()
+{
 	//check for Physics Handle Component
 	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if(PhysicsHandle) UE_LOG(LogTemp, Warning, TEXT("Physics handle component found"))
 	if(!PhysicsHandle) UE_LOG(LogTemp, Error, TEXT("The following object does not have a Physics Handle Component attached to it: %s"), *GetOwner()->GetName())
+}
 
+void UGrabber::SetupInputComponent()
+{
 	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
 
 	if(InputComponent) 
@@ -40,18 +46,31 @@ void UGrabber::BeginPlay()
 void UGrabber::Grab()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Grab key pressed."))
+
+	GetFirstPhysicsBodyInReach();//only raycast when key is pressed and see if we reach any actors with physics body collision channel set
+
+	//if we hit something then attach the physics handle
+	//TODO attach physics handle
 }
 
 void UGrabber::Release()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Grab key released."))
+	//TODO remove/release the physics handle
 }
 
 // Called every frame
+//TickComponent is a "hot loop" because it is called often
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	//if the physics handle is attached, move the object we are holding
+
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const
+{
 	//Get player's viewpoint
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
@@ -90,4 +109,6 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 
 	//log out to test
 	if(ActorHit) UE_LOG(LogTemp, Warning, TEXT("Line trace has hit: %s"), *ActorHit->GetName())
+
+	return Hit;
 }
--- Source/Building_Escape/Grabber.h
+++ Source/Building_Escape/Grabber.h
@@ -15,20 +15,24 @@ class BUILDING_ESCAPE_API UGrabber : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UGrabber();
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
 private:
+	//attributes/variables
 	float Reach = 100.f;
 	UPhysicsHandleComponent* PhysicsHandle = nullptr;
 	UInputComponent* InputComponent = nullptr;
 
+	//methods
 	void Grab();
 	void Release();
+	void FindPhysicsHandle();
+	void SetupInputComponent();
+	//return the first actor within reach with physics body
+	FHitResult GetFirstPhysicsBodyInReach() const;
 };
